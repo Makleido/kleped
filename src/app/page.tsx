44f0f9dbf385a197703a5fce0a4aede5d11c6144
Makleido <kagethk@gmail.com>
2024-04-21 15:28:07 +0200

    Nextjs initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-[calc(100vh)]">
+      <h1 className="font-bold text-3xl">Hello world!</h1>
+    </main>
+  );
+}
